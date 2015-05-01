8170ba153e82979a1119014f40f5df945efadcc9
mnuthai <mnuthai@gmail.com>
2015-04-30 22:48:07 -0700

    images uploaded must be less than 5MB

--- app/scripts/services/userservice.js
+++ app/scripts/services/userservice.js
@@ -33,9 +33,7 @@ angular.module('projectsApp')
 
     var saveMoreSettings = function(user, imageSrc) {
       console.log('saving more info...');
-      console.log(user);  
       if(user !== undefined){
-        console.log(user);  
         // update the user with additional info that was submitted  
         if(user.birthday !== undefined){
           ref.child('profileInfo').child(authData.uid).update({
@@ -75,16 +73,18 @@ angular.module('projectsApp')
       }
     };
 
-    function readImage(input) {
-        if ( input.files && input.files[0] ) {
-            var FR= new FileReader();
-            FR.onload = function(e) {
-                 $('#img').attr( 'src', e.target.result );
-                 $('#base').text( e.target.result );
-            };
-            FR.readAsDataURL( input.files[0] );
-        }
-    }
+    /*
+     *function readImage(input) {
+     *    if ( input.files && input.files[0] ) {
+     *        var FR= new FileReader();
+     *        FR.onload = function(e) {
+     *             $('#img').attr( 'src', e.target.result );
+     *             $('#base').text( e.target.result );
+     *        };
+     *        FR.readAsDataURL( input.files[0] );
+     *    }
+     *}
+     */
 
     var setUserProvision = function() {
       var provisionedData = ref.child('privacySettings').child(authData.uid);
@@ -127,56 +127,56 @@ angular.module('projectsApp')
         }
       };
 
-    var clientId = '824361687622-oigige156t3n418c8p14or24pqdqrdkq.apps.googleusercontent.com';
-    var scopes = 'https://www.googleapis.com/auth/plus.me';
-    var googleImport = function(){
-      console.log('...requesting deeper google auth...');
-      var apiKey = 'AIzaSyAAY3m6JlU7DVn5GdNMcilJ0jP7qW7p7PI';
-      gapi.client.setApiKey(apiKey);
-      gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true}, handleAuthResult);
-    };
+      var clientId = '824361687622-oigige156t3n418c8p14or24pqdqrdkq.apps.googleusercontent.com';
+      var scopes = 'https://www.googleapis.com/auth/plus.me';
+      var googleImport = function(){
+        console.log('...requesting deeper google auth...');
+        var apiKey = 'AIzaSyAAY3m6JlU7DVn5GdNMcilJ0jP7qW7p7PI';
+        gapi.client.setApiKey(apiKey);
+        gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true}, handleAuthResult);
+      };
 
-   var facebookImport = function() {
-        ref.authWithOAuthPopup('facebook', function(error, authData) {
-          if (error) {
-            console.log('Login Failed!', error);
-          } else {
+      var facebookImport = function() {
+          ref.authWithOAuthPopup('facebook', function(error, authData) {
+            if (error) {
+              console.log('Login Failed!', error);
+            } else {
 
-            console.log("Authenticated successfully with payload:", authData);
-
-
-           Facebook.api('/me', function(response) {
-              $scope.user = response;
-              console.log("FirstName: " + response.first_name + 
-                " LastName: " + response.last_name + 
-                " Gender: " + response.gender + 
-                " Birthday: " + response.birthday + 
-                " SchoolName: " + response.education[1].school.name + 
-                " Concentration: " + response.education[1].concentration[0].name + 
-                " Year: " + response.education[1].year.name + 
-                " FavoriteTeam: " + response.favorite_teams[0].name);
+              console.log("Authenticated successfully with payload:", authData);
 
 
-              if(response.birthday !== undefined){
-                console.log(response.birthday);
-                $scope.$apply(function() {
-                  $scope.user.birthday = new Date(response.birthday);
-                });
-              }
+             Facebook.api('/me', function(response) {
+                $scope.user = response;
+                console.log("FirstName: " + response.first_name + 
+                  " LastName: " + response.last_name + 
+                  " Gender: " + response.gender + 
+                  " Birthday: " + response.birthday + 
+                  " SchoolName: " + response.education[1].school.name + 
+                  " Concentration: " + response.education[1].concentration[0].name + 
+                  " Year: " + response.education[1].year.name + 
+                  " FavoriteTeam: " + response.favorite_teams[0].name);
 
-              if(response.education[1].school.name !== undefined){
-                console.log(response.education[1].school.name);
-                $scope.$apply(function() {
-                  $scope.user.school = response.education[1].school.name;
-                });
-              }
-               
 
+                if(response.birthday !== undefined){
+                  console.log(response.birthday);
+                  $scope.$apply(function() {
+                    $scope.user.birthday = new Date(response.birthday);
+                  });
+                }
+
+                if(response.education[1].school.name !== undefined){
+                  console.log(response.education[1].school.name);
+                  $scope.$apply(function() {
+                    $scope.user.school = response.education[1].school.name;
+                  });
+                }
+                 
+
+              });
+            }
+          }, {
+              scope: "user_likes,email,user_birthday,public_profile,user_education_history,user_about_me" // permission requests
             });
-          }
-        }, {
-            scope: "user_likes,email,user_birthday,public_profile,user_education_history,user_about_me" // permission requests
-          });
       };
 
       var handleAuthResult = function(authResult) {
@@ -218,22 +218,21 @@ angular.module('projectsApp')
         });
       };
 
-      $scope.uploadImage = function(image) {
-        readImage(image);
-        console.log('uploading image...');
-      };
-
       $scope.getFile = function (file) {
-      $scope.progress = 0;
-      fileReader.readAsDataUrl(file, $scope)
-                    .then(function(result) {
-                        $scope.imageSrc = result;
-                    });
+        // check if file size is 5MB+
+        $scope.invalidFile = false;
+        $scope.invalidText = "";
+        if (file.size > 5000000) {
+          $scope.invalidFile = true;
+          $scope.invalidText = "File size is too big!!!";
+          $scope.$apply();
+        } else {
+        fileReader.readAsDataUrl(file, $scope)
+                      .then(function(result) {
+                          $scope.imageSrc = result;
+                      });
+        }
       };
-   
-      $scope.$on("fileProgress", function(e, progress) {
-          $scope.progress = progress.loaded / progress.total;
-      });
     }
 
     var showAboutForm = function() {
@@ -252,7 +251,7 @@ angular.module('projectsApp')
 
     return {
       getMoreUserInfo: function(provisionSettings) {
-        console.log(provisionSettings);
+        console.log("provisionSettings: " + provisionSettings);
         if (provisionSettings == '0') {
           showAboutForm();
         }
