004cfe715cb718fee877e5e2ed75dd388c08c2b4
Dennis Nguyen <dnguy078@ucr.edu>
2015-05-12 11:15:40 -0700

    refactored profile

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -38,7 +38,17 @@ angular
     });
   }])
   .config(function ($urlRouterProvider, $stateProvider,  $mdThemingProvider, FacebookProvider) {
-    $mdThemingProvider.theme('default');//.light();//.dark();
+    $mdThemingProvider.theme('default')
+      .primaryPalette('indigo', {
+        'default': '600', // by default use shade 400 from the pink palette for primary intentions
+        'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+        'hue-2': '400', // use shade 600 for the <code>md-hue-2</code> class
+        'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+      });
+
+
+
+    //.light();//.dark();
     $mdThemingProvider.theme('altTheme').primaryPalette('purple'); // specify primary color, all
                             // other color intentions will be inherited
                             // from default
