26adb0a67cabcc0caaeab3c4301eeffa32249fa1
Dennis Nguyen <dnguy078@ucr.edu>
2015-05-25 12:39:03 -0700

    formatting

--- app/scripts/services/alertservice.js
+++ app/scripts/services/alertservice.js
@@ -10,45 +10,43 @@
 // AngularJS will instantiate a singleton by calling "new" on this function
 angular.module('projectsApp')
   .service('alertService', function ($mdDialog, firebaseService, userService, $state) {
-      return {
-        show: function(title,msg) {
-        $mdDialog.show(
-          $mdDialog.alert()
-            .title(title)
-            .content(msg)
-            .ariaLabel('Alert Dialog')
-            .ok('close')
-        );
-        },
-        removeAccount: function($scope, email, password, uid) {
+    return {
+      show: function(title,msg) {
+      $mdDialog.show(
+        $mdDialog.alert()
+          .title(title)
+          .content(msg)
+          .ariaLabel('Alert Dialog')
+          .ok('close')
+      );
+      },
+      removeAccount: function($scope, email, password, uid) {
+        var ref = new Firebase(firebaseService.getFirebBaseURL());
 
-            var ref = new Firebase(firebaseService.getFirebBaseURL());
+        var confirm = $mdDialog.confirm()
+        .title('Delete Account')
+        .content('WARNING!! Are you sure you want to delete your account?')
+        .ariaLabel('test ')
+        .ok('Yes, I agree.')
+        .cancel('Cancel')
 
-            var confirm = $mdDialog.confirm()
-            .title('Delete Account')
-            .content('WARNING!! Are you sure you want to delete your account?')
-            .ariaLabel('test ')
-            .ok('Yes, I agree.')
-            .cancel('Cancel')
+        $mdDialog.show(confirm).then(function() {
+          //pressed yes
+          userService.deleteCurrentUser(email, password, uid);
 
-            $mdDialog.show(confirm).then(function() {
-              //pressed yes
-              userService.deleteCurrentUser(email, password, uid);
+          //logout
+          console.log('Logging Out!')
+          ref.child('profileInfo').child(uid).update({
+            loggedIn: false
+          });
+          localStorage.clear();
+          ref.unauth();
+          $state.go('login');
 
-              //logout
-              console.log('Logging Out!')
-              ref.child('profileInfo').child(uid).update({
-                loggedIn: false
-              });
-              localStorage.clear();
-              ref.unauth();
-              $state.go('login');
-              
-            }, function() {
-              //cancelled
-              
-            });
+        }, function() {
+          //cancelled
 
-        }
-      };
-  });
+        });
+      }
+    };
+});
