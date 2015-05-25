054c9ebb52bba5ce718e8b1f4e64a8c192e3761b
Dennis Nguyen <dnguy078@ucr.edu>
2015-05-25 13:45:53 -0700

    chat everyone

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -2,74 +2,17 @@
 
 angular.module('projectsApp')
 .controller('DashboardController',  function($scope, $http, $firebaseAuth, $firebaseArray, $firebaseObject , firebaseService, profileService, chatService) {
-
   var ref = new Firebase(firebaseService.getFirebBaseURL());
   var authObj = $firebaseAuth(ref);
   var authData = authObj.$getAuth();
   console.log('Dashboard Logged in as:', authData.uid);
 
   $scope.friendProfile = [];
-  //gets list of all current users friends
-  var friendsRef = new Firebase('https://shining-torch-23.firebaseio.com/friends/'+ authData.uid +'/friendList');
-
-  //for each person in the friends array, loop through the array to get user profile
-  var list = $firebaseArray(friendsRef);
-  var friendProfileArr = [];
-  list.$loaded(
-  function(x) {
-    //loops through and gets profile data, adds to friendprofile array
-    //friend profile array contains array of userprofileData objects
-    x.forEach(function(entry) {
-      getUserProfileInfo(entry.$id);
-    });
-    // $scope.friendProfiles = friendProfile;
-    }, function(error) {
-        console.error('Error:', error);
-  });
-
-  //gets user profile info
-  var getUserProfileInfo = function(userid) {
-    var userProfileRef = new Firebase('https://shining-torch-23.firebaseio.com/profileInfo/'+ userid);
-    var profileData = $firebaseObject(userProfileRef);
-    profileData.$loaded(
-      function(data) {
-        friendProfileArr.push(data);
-      },
-      function(error) {
-        console.error('Error:', error);
-      }
-    );
-    $scope.friendProfiles = friendProfileArr;
-  };
+  var friendsRef = new Firebase('https://shining-torch-23.firebaseio.com/profileInfo');
+  $scope.friendProfiles  = $firebaseArray(friendsRef);
 
   $scope.clicked = function(friendUID) {
-    // console.log("clicked" + friendUID);
+    //chatService used to add friendUID to recent chat page
     chatService.addFriendChat(friendUID);
-    // communicate to service
-
-
-
-
   };
-
-  //Chat
-  // var chatRef = new Firebase('https://shining-torch-23.firebaseio.com/chats/'+ authData.uid);
-  // $scope.chatList = $firebaseArray(chatRef);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
